8d3f46289dae6699972579127b6e760a25f00b24
Xinjie Lin <eyjafjala@foxmail.com>
2025-06-06 18:12:31 +0800

    lab3 first

--- src/execution/insert_executor.cpp
+++ src/execution/insert_executor.cpp
@@ -18,10 +18,20 @@ namespace bustub {
 
 InsertExecutor::InsertExecutor(ExecutorContext *exec_ctx, const InsertPlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void InsertExecutor::Init() { throw NotImplementedException("InsertExecutor is not implemented"); }
+void InsertExecutor::Init() {
+    table_info_ = exec_ctx_->GetCatalog()->GetTable(plan_->GetTableOid());
+    indexes_ = exec_ctx_->GetCatalog()->GetTableIndexes(table_info_->name_);
+    child_executor_->Init();
+}
 
-auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool { return false; }
+auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
+    bool is_finished = child_executor_->Next(tuple, rid);
+    if (is_finished) {
+
+    }
+    return is_finished;
+}
 
 }  // namespace bustub
--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -14,10 +14,31 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx), plan_(plan) {}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() { 
+  table_info_ = exec_ctx_->GetCatalog()->GetTable(plan_->GetTableOid());
+  iter_ = std::make_unique<TableIterator>(table_info_->table_->MakeIterator());
+ }
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { 
+    while (true) {
+      if (iter_->IsEnd()) {
+        return false;
+      }
+      auto tp = iter_->GetTuple();
+      *tuple = tp.second;
+      *rid = iter_->GetRID();
+      ++(*iter_);
+      if (tp.first.is_deleted_) {
+        continue;
+      }
+      if (plan_->filter_predicate_ != nullptr && !plan_->filter_predicate_->Evaluate(tuple, table_info_->schema_).GetAs<bool>()) {
+        continue;
+      }
+      return true;
+    }
+    return false;
+ }
 
 }  // namespace bustub
